1bfcfc18c6e9cbcec4c3bb3497e1953289e5f055
Kyle Brandon <kyle_brandon@sfu.ca>
2024-04-11 21:15:20 -0700

    Delete project directory

--- project/test.py
+++
@@ -1,6 +0,0 @@
-from transformers import DistilBertForSequenceClassification
-
-model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=6)
-
-# After training the model, save it
-model.save_pretrained('./toxic_comment_model')
